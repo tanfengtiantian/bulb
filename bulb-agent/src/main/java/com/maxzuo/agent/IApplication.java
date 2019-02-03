351b2c90c2eecfd87410341d14951ab943fae012
dazuo <fuhongzuo@gmail.com>
2019-02-03 15:48:06 +0800

    探针同步

---
+++ bulb-agent/src/main/java/com/maxzuo/agent/IApplication.java
@@ -0,0 +1,13 @@
+package com.maxzuo.agent;
+
+/**
+ * Created by zfh on 2019/01/30
+ */
+public interface IApplication {
+
+    public void init();
+
+    public void execute();
+
+    public void destory();
+}
--- bulb-agent/src/main/java/com/maxzuo/agent/Main.java
+++ bulb-agent/src/main/java/com/maxzuo/agent/Main.java
@@ -3,27 +3,14 @@ package com.maxzuo.agent;
 import java.lang.instrument.Instrumentation;
 
 /**
- * JavaAgent（代理）类
- * Created by zfh on 2019/01/20
+ * 使用探针实现服务器热部署
+ * Created by zfh on 2019/01/30
  */
 public class Main {
 
-    /**
-     * 该方法在main方法之前运行，与main方法运行在同一个JVM中
-     * 并被同一个System ClassLoader装载
-     * 被统一的安全策略(security policy)和上下文(context)管理
-     */
-    public static void premain(String agentOps, Instrumentation inst) {
-        System.out.println("=========premain方法执行========");
-        System.out.println(agentOps);
-    }
-
-    /**
-     * 如果不存在 premain(String agentOps, Instrumentation inst)
-     * 则会执行 premain(String agentOps)
-     */
-    public static void premain(String agentOps) {
-        System.out.println("=========premain方法执行2========");
-        System.out.println(agentOps);
+    public static void premain(String options, Instrumentation inst) {
+        System.out.println("========进入premain方法============");
+        System.out.println("options：" + options);
+        //inst.addTransformer();
     }
 }
