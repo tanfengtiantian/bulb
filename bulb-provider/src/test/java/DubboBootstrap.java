c09cdc2c24c7e02199e4ddbb2e55d3810a7b4894
zfh <fuhongzuo@gmail.com>
2019-03-09 20:13:59 +0800

    启动原理-添加注释说明

--- bulb-provider/src/test/java/DubboBootstrap.java
+++ bulb-provider/src/test/java/DubboBootstrap.java
@@ -6,6 +6,8 @@ public class DubboBootstrap {
 
     public static void main(String[] args) {
         System.out.println("bulb-order-provider start...");
+
+        // Main中会根据配置加载容器模块
         com.alibaba.dubbo.container.Main.main(args);
     }
 }
