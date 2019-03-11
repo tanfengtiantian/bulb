522ed5bc9a31ae042b8c4f6b62a1cad850e21712
zfh <fuhongzuo@gmail.com>
2019-03-11 19:38:34 +0800

    配置日志输出路径

---
+++ spring-boot-app/src/main/java/com/maxzuo/elastic/ESClientTest.java
@@ -0,0 +1,29 @@
+package com.maxzuo.elastic;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.elasticsearch.action.get.GetResponse;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * ES客户端基本使用
+ * Created by zfh on 2019/03/11
+ */
+public class ESClientTest {
+
+    @Test
+    public void queryByPrimaryKey() {
+        GetResponse response = ElasticSearchUtils.getRecordByPrimaryKey("zxcity_elk", "file", "AWlq3_I-P9zaf8kQeL0a");
+        System.out.println("result：" + response.getSource());
+    }
+
+    @Test
+    public void saveRecord () {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "dazuo");
+        jsonObject.put("profession", "java工程师");
+        Boolean aBoolean = ElasticSearchUtils.saveRecord("zxcity_test", "file", JSON.toJSONString(jsonObject));
+        Assert.assertTrue(aBoolean);
+    }
+}
---
+++ spring-boot-app/src/main/java/com/maxzuo/elastic/ElasticSearchUtils.java
@@ -0,0 +1,72 @@
+package com.maxzuo.elastic;
+
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+
+/**
+ * ES工具类
+ * Created by zfh on 2019/01/24
+ */
+public class ElasticSearchUtils {
+
+    private static final Logger    logger     = LoggerFactory.getLogger(ElasticSearchUtils.class);
+
+    private static final String ES_IP = "47.98.199.80";
+
+    private static final Integer ES_PROT = 9300;
+
+    private static TransportClient client;
+
+    static {
+        Settings settings = Settings.settingsBuilder().put("client.transport.ignore_cluster_name", true)
+            .put("client.transport.sniff", true).build();
+        try {
+            client = TransportClient.builder().settings(settings).build()
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(ES_IP), ES_PROT));
+        } catch (Exception e) {
+            logger.error("ES连接异常",e);
+        }
+    }
+
+    /**
+     * 通过ID查询数据
+     * @param index 索引
+     * @param type 类型
+     * @param id 记录ID
+     * @return {@link GetResponse}
+     */
+    public static GetResponse getRecordByPrimaryKey(String index, String type, String id) {
+        GetResponse response = null;
+        if (client != null) {
+            response = client.prepareGet(index, type, id).get();
+        }
+        return response;
+    }
+
+    /**
+     * 保存一条记录
+     * @param index 索引名
+     * @param type  类型
+     * @param document 文档
+     * @return true or false
+     */
+    public static Boolean saveRecord(String index, String type, String document) {
+        if (client != null) {
+            return client.prepareIndex(index, type).setSource(document).get().isCreated();
+        }
+        return false;
+    }
+
+    /** 关闭ES连接 */
+    public static void close() {
+        if (client != null) {
+            client.close();
+        }
+    }
+}
