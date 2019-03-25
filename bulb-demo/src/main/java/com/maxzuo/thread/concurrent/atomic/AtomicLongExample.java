4c0b64fc9fa580a9d7724de419369bc1bb36a58d
zfh <fuhongzuo@gmail.com>
2019-03-25 13:02:17 +0800

    新增 AtomicLong示例

---
+++ bulb-demo/src/main/java/com/maxzuo/thread/concurrent/atomic/AtomicLongExample.java
@@ -0,0 +1,35 @@
+package com.maxzuo.thread.concurrent.atomic;
+
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * AtomicLong原子类
+ * Created by zfh on 2019/03/25
+ */
+public class AtomicLongExample {
+
+    private static final AtomicLong atomicLong = new AtomicLong(10);
+
+    /**
+     * 获取前一个值
+     */
+    @Test
+    public void testPreviousValue () {
+        System.out.println("当前值：" + atomicLong.get());
+        long andIncrement = atomicLong.getAndIncrement();
+        System.out.println("previous value: " + andIncrement);
+        System.out.println("当前值：" + atomicLong.get());
+    }
+
+    /**
+     * 更新并返回当前值
+     */
+    @Test
+    public void testUpdateValue () {
+        long currentValue = atomicLong.incrementAndGet();
+        System.out.println("currentValue：" + currentValue);
+        System.out.println("get currentValue: " + atomicLong.get());
+    }
+}
