f0436bc9f27f21cb5b5c0c82e3c71abdeefa8b56
zfh <fuhongzuo@gmail.com>
2019-04-04 11:52:50 +0800

    更新Condition使用示例

--- bulb-demo/src/main/java/com/maxzuo/juc/locks/ConditionExample.java
+++ bulb-demo/src/main/java/com/maxzuo/juc/locks/ConditionExample.java
@@ -19,10 +19,15 @@ public class ConditionExample {
 
     private static final ReentrantLock LOCK = new ReentrantLock();
 
-    private static final Condition     STOP = LOCK.newCondition();
+    private static final Condition     STOP_ONE = LOCK.newCondition();
 
+    private static final Condition     STOP_TWO = LOCK.newCondition();
+
+    /**
+     * 测试多个Condition控制多线程的休眠与唤醒
+     */
     public static void main(String[] args) {
-        // TODO: 使用线程池会有问题？
+        // TODO: 使用线程池会有问题！！！  使用junit会有问题！！！
 
         // 线程1
         Thread t1 = new Thread(new Runnable() {
@@ -32,7 +37,18 @@ public class ConditionExample {
 
                 try {
                     LOCK.lock();
-                    STOP.await();
+                    // 等待 STOP_ONE 唤醒，当STOP_ONE.signal()执行后，并不会立即结束STOP_ONE.awit() ，需要等待其LOCK.unlock()
+                    STOP_ONE.await();
+
+                    System.out.println("STOP_ONE await 结束 ... ");
+
+                    // 等待 STOP_TWO 环境；必须依次唤醒，否则t1一直等待下去
+
+                    STOP_TWO.await();
+
+                    System.out.println("STOP_TWO await 结束 ... ");
+
+                    TimeUnit.SECONDS.sleep(2);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
@@ -44,7 +60,7 @@ public class ConditionExample {
         });
         t1.start();
 
-        // 线程2
+        // 线程2，使用STOP_ONE
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -53,8 +69,15 @@ public class ConditionExample {
                     TimeUnit.SECONDS.sleep(2);
 
                     LOCK.lock();
-                    STOP.signal();
+                    STOP_ONE.signal();
 
+                    System.out.println("STOP_ONE signal 触发完毕 ... ");
+
+                    System.out.println("t2 进入第二次等待 ... ");
+
+                    TimeUnit.SECONDS.sleep(2);
+
+                    System.out.println("t2 第二次 等待结束 ... ");
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
@@ -65,6 +88,26 @@ public class ConditionExample {
         });
         t2.start();
 
-        System.out.println("hello condition");
+        // 线程3，使用STOP_TWO
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " -------- hello t3");
+                try {
+                    TimeUnit.SECONDS.sleep(8);
+                    LOCK.lock();
+                    STOP_TWO.signal();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    LOCK.unlock();
+                }
+                System.out.println("t3 end ...");
+            }
+        });
+        t3.start();
+
+        System.out.println("main thread ... ");
     }
 }
