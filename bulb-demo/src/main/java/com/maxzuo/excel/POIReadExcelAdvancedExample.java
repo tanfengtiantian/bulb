c5dff9fd9fe89640b261e0a6007cd36a3c105dec
zfh <fuhongzuo@gmail.com>
2019-05-09 19:07:25 +0800

    扩展-将Excel文件上传至OSS

--- bulb-demo/src/main/java/com/maxzuo/excel/POIReadExcelAdvancedExample.java
+++ bulb-demo/src/main/java/com/maxzuo/excel/POIReadExcelAdvancedExample.java
@@ -1,5 +1,7 @@
 package com.maxzuo.excel;
 
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.ObjectMetadata;
 import org.apache.poi.hssf.converter.ExcelToHtmlConverter;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -15,7 +17,9 @@ import java.net.URL;
 
 /**
  * 高级版：POI克隆Excel模板，转换成HTML；不使用临时文件，通过ByteArrayInputStream将输出流转换为字节，不读取磁盘，提高效率。
- * <p>
+ * <pre>
+ *  扩展：将Excel文件上传至OSS，获取文件存储路径
+ * </pre>
  * Created by zfh on 2019/05/09
  */
 public class POIReadExcelAdvancedExample {
@@ -24,11 +28,18 @@ public class POIReadExcelAdvancedExample {
         URL templateResource = POIReadExcelExample.class.getResource("template/applyTableTemplate.xls");
         try {
             // 通过模板克隆Excel，填充数据
-            InputStream inputStream = cloneExcelFile(templateResource.getFile());
+            byte[] dataArray = cloneExcelFile(templateResource.getFile());
 
             // 将Excel转换成Html
-            String htmlFilePath ="F:\\bulb\\bulb-demo\\applyTableTemplate.html";
-            convertExcelToHtml(inputStream, htmlFilePath);
+            String htmlFilePath = "F:\\bulb\\bulb-demo\\applyTableTemplate.html";
+            convertExcelToHtml(new ByteArrayInputStream(dataArray), htmlFilePath);
+
+            // 扩展存储文件到本地
+            String excelFilePath = "F:\\bulb\\bulb-demo\\applyTableTemplate.xls";
+            saveFileToLocal(new ByteArrayInputStream(dataArray), excelFilePath);
+
+            // 扩展：上传文件至OSS
+            uploadFileToOSS(new ByteArrayInputStream(dataArray));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -37,7 +48,7 @@ public class POIReadExcelAdvancedExample {
     /**
      * 克隆模板，填充模板数据，获取最终的文件（通过字节数组，将输出流转换成输入流）
      */
-    private static InputStream cloneExcelFile(String filePath) throws Exception {
+    private static byte[] cloneExcelFile(String filePath) throws Exception {
         HSSFWorkbook wb = new HSSFWorkbook(new FileInputStream(filePath));
         HSSFSheet sheet = wb.cloneSheet(0);
         wb.setSheetName(0, "Sheet1");
@@ -52,7 +63,7 @@ public class POIReadExcelAdvancedExample {
         wb.write(baos);
         baos.flush();
         baos.close();
-        return new ByteArrayInputStream(baos.toByteArray());
+        return baos.toByteArray();
     }
 
     /**
@@ -134,5 +145,66 @@ public class POIReadExcelAdvancedExample {
         FileOutputStream out = new FileOutputStream(htmlFilePath);
         out.write(writer.toString().getBytes("UTF-8"));
         out.flush();
+        hssfWorkbook.close();
+    }
+
+    /**
+     * 扩展：保存文件到本地
+     */
+    private static void saveFileToLocal (InputStream is, String filePath) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(new File(filePath));
+            byte[] carrier = new byte[1024];
+            int count;
+            while ((count = is.read(carrier)) != -1) {
+                fos.write(carrier, 0 ,count);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 扩展：上传文件至OSS服务器
+     */
+    private static void uploadFileToOSS (InputStream is) {
+        String accessKeyId = "AFNeMLmCjpYszhLU";
+        String accessKeySecret = "qSWKSPe8zBfjK7UJA7Q90mYAskgiru";
+        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+        String endpointSave = "https://oss-cn-hangzhou.aliyuncs.com";
+
+        // 阿里云上创建
+        String bucketName = "account-excel";
+        // 文件目录
+        String fileDirectory = "fans-activity-table/";
+        // 文件名
+        String fileName = "20170000-applyTable.xls";
+
+        OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+        ObjectMetadata meta = new ObjectMetadata();
+        meta.setContentType("application/vnd.ms-excel");
+        client.putObject(bucketName, fileDirectory + fileName, is, meta);
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("文件路径：" + endpointSave + "/" + bucketName + "/" + fileDirectory + fileName);
     }
 }
